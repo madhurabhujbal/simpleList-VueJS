3e1968fc3e6b21632d692c7f84c52ea2cd85ff2f
Madhura <madhura.bhujbal@gmail.com>
2020-11-13 15:21:55 +0000

    Create a new component

---
+++ src/components/WeekDays.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="main">
+      <h2>Hello world! Now</h2>
+      <!-- <li v-for="(item, id) in listItems" v-bind:key="id">
+    {{item.title}}
+  </li> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'WeekDay'
+//     data() {
+//     return {
+//       listItems: [
+//         {id: 1, title: "Sunday"},
+//         {id: 2, title: "Monday"},
+//         {id: 3, title: "Tuesday"}
+//       ]
+//     }
+//   },
+
+}
+</script>
+
+<style>
+    h2 {
+        margin-top: 5%;
+        margin-bottom: 5%;
+    }
+
+</style>
