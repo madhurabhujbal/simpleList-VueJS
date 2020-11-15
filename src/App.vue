5c98788145fae7d557ce0a2e637835a12d931a66
Madhura <madhura.bhujbal@gmail.com>
2020-11-15 18:16:46 +0000

    Move the list and code to render it in component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
 <div id="app">
-  <ul v-for="(item, id) in listItems" v-bind:key="id">
     <li><WeekDay /></li>
-  </ul>
   <button @click="buttonClicked">Button</button>
 </div>
 </template>
@@ -17,21 +15,10 @@ export default {
       this.title = "button clicked"
     }
   },
-  data() {
-    return {
-      listItems: [
-        {id: 1, title: "Sunday"},
-        {id: 2, title: "Monday"},
-        {id: 3, title: "Tuesday"}
-      ]
-    }
-  },
 
 }
 </script>
 
 <style>
-/* button {
-  margin-top: 30%;
-} */
+
 </style>
