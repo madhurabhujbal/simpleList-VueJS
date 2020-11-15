3f24d37b5e4389a8eacf9d2fd51ee553762bb686
Madhura <madhura.bhujbal@gmail.com>
2020-11-15 18:15:32 +0000

    Add code to pass and render hard coded list from component

--- src/components/WeekDay.vue
+++ src/components/WeekDay.vue
@@ -1,10 +1,23 @@
 <template>
-    <h2>Hello world! Now</h2>
+  <div>
+    <ol v-for="(item, id) in listItems" v-bind:key="id">
+      <h3> {{listItems[id].title}} </h3>
+    </ol>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'WeekDay'
+    name: 'WeekDay',
+    data() {
+    return {
+      listItems: [
+        {id: 1, title: "Sunday"},
+        {id: 2, title: "Monday"},
+        {id: 3, title: "Tuesday"}
+      ]
+    }
+  },
 }
 </script>
 
