d6023886924afeed9dce89c49e1c6958fecd9ab5
Madhura <madhura.bhujbal@gmail.com>
2020-11-22 16:44:46 +0000

    Add all necessary bootstrap imports files

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { Vue, createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import App from './App.vue';
 
-import { BootstrapVue } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 createApp(App).mount('#app')
